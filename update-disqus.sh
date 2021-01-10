aba3dba80d6e0ab2c3b5137cd65bb7e13d3ab4c2
Shawn Dennis Lin <shawndennislin@gmail.com>
2021-01-10 23:17:02 +0800

    [fix] Disqus

---
+++ update-disqus.sh
@@ -0,0 +1,5 @@
+cp ./themes/toha/layouts/_default/single.html ./layouts/_default/
+echo "cp ./themes/toha/layouts/_default/single.html ./layouts/_default/"
+
+cp ./themes/toha/layouts/partials/disqus.html ./layouts/partials/
+echo "cp ./themes/toha/layouts/partials/disqus.html ./layouts/partials/"
